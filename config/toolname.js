6b7485f131ab71616f933301e08015e183666760
KOSASIH <kosasihg88@gmail.com>
2024-06-20 23:29:43 +0700

    Create toolname.js

---
+++ config/toolname.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "Blueprint": "Blueprint'/Game/PrimalEarth/CoreBlueprints/Weapons/PrimalItem_TekRifle.PrimalItem_TekRifle'",
+  "Player": {
+    "AllowDamage": true,
+    "AllowDamageText": true,
+    "MaxDistance": -1,
+    "DamageMultiplier": 1,
+    "MaxDamage": -1
+  },
+  "TamedDino": {
+    "AllowDamage": false,
+    "AllowDamageText": true,
+    "MaxDistance": -1,
+    "DamageMultiplier": 1,
+    "MaxDamage": -1
+  },
+  "WildDino": {
+    "AllowDamage": true,
+    "AllowDamageText": true,
+    "MaxDistance": -1,
+    "DamageMultiplier": 1,
+    "MaxDamage": -1
+  },
+  "Structure": {
+    "AllowDamage": true,
+    "AllowDamageText": true,
+    "MaxDistance": 1000,
+    "DamageMultiplier": 1,
+    "MaxDamage": -1
+  }
+};
