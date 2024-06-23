498dbca8d3863b4133c40054d62c532073143d0c
KOSASIH <kosasihg88@gmail.com>
2024-06-23 18:46:42 +0700

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WalletConnectButton from './components/WalletConnectButton';
+
+function App() {
+  return (
+    <div>
+      <h1>SilkRoad Marketplace</h1>
+      <WalletConnectButton />
+      <!-- Rest of the UI -->
+    </div>
+  );
+}
+
+export default App;
